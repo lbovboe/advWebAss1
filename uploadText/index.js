1d535f8860046a03da9270db29a2cb62420adf40
paul <jianbo2@hotmail.com>
2020-06-12 19:45:09 +0800

    manage files done, only left with search degree

--- uploadText/index.js
+++
@@ -1,9 +0,0 @@
-var router = require("./router");
-var server = require("./server");
-var handlers = require("./requestHandler");
-var handle = {};
-
-handle["/"] = handlers.reqStart;
-handle["/start"] = handlers.reqStart;
-handle["/upload"] = handlers.reqUpload;
-server.startServer(41015, router.route, handle);
